4b60027f705388ca03c493a96076c6404e6aeb5f
KimFlowering <kyhyun1999@gmail.com>
2018-07-17 23:38:46 +0900

    three dat commit

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ nodecoin, this most awesome coin maid in NodeJS,
 
 2018-07-16
 use Crypto-JS to Hashing coin
+
+2018-07-17
+coin valid and resue check
--- src/blockchain.js
+++ src/blockchain.js
@@ -26,7 +26,8 @@ const getLastBlock = () => blockChain[blockChain.length -1];
 const getTimeStamp = () => new Date().getTime() / 1000;
 
 const createHash = (index, previousHash, timestamp, data) => {
-    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    CryptoJS.SHA256(index + previousHash + timestamp + JSON.stringify(data)
+    ).toString();
 
 }
 
@@ -44,3 +45,29 @@ const createNewBlock = data => {
     );
     return newBlock;
 };
+
+const getBlockHash = (block) => createHash(block.index, block.previousHash, block.timestamp, block.data);
+
+const isNewBlockValid = (candidateBlock, latestBlock) => {
+    if(latestBlock + 1 !== candidateBlock.index){
+        console.log('The Candidate blcok doesnt have a valid index');
+        return false;
+    }else if(latestBlock.hash !== candidateBlock.previousHash){
+        console.log('The previousHash of the candidate block is not the hash of the lastest block');
+        return false;
+    }else if(getBlockHash(candidateBlock) !== candidateBlock.hash){
+        console.log('The hash of the block is invaild');
+        return false;
+    }
+    return true;
+}
+
+const isNewStructurValid = () => {
+    return (
+        typeof block.index == 'number' && 
+        typeof block.hash == "string" && 
+        typeof block.previousHash == "string"&& 
+        typeof block.timestamp == "date" &&
+        typeof block.data == "string"
+    )
+}
